abfaa013b499177931b38e0f18f8107f84fd6438
NITHYAPRIYA M <142155664+nithyapriya-din@users.noreply.github.com>
2024-03-02 21:17:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavDesign from './Components/NavDesign';
+import CardDesign from './Components/CardDesign';
+
+
+function App() {
+  return (
+    <div>
+      <NavDesign></NavDesign>
+      <CardDesign></CardDesign>
+    </div>
+  );
+}
+
+export default App;
